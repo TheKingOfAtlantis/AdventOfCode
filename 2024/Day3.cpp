688d9645c81dc331c13c8dcd4a6ea24b8539e67e
Sulaiman Sulaiman <SKSulai98@gmail.com>
2024-12-03 10:32:28 +0000

    2024 - Day3: Setup

---
+++ 2024/Day3.cpp
@@ -0,0 +1,18 @@
+
+// https://adventofcode.com/2024/day/3
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include <ranges>
+#include <algorithm>
+#include <numeric>
+#include <span>
+#include <vector>
+#include <unordered_map>
+
+int main() {
+    
+    return 0;
+}
