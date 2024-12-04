565db299cf6329aa485f50621b9dcda5b5bbcb7f
Sulaiman Sulaiman <SKSulai98@gmail.com>
2024-12-04 10:30:51 +0000

    Year 2024 - Day 4: Setup

---
+++ 2024/Day04.cpp
@@ -0,0 +1,12 @@
+
+// https://adventofcode.com/2024/day/4
+
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string input;
+    while(std::getline(std::cin, input) && !input.empty());
+
+    return 0;
+}
