c96908caf6702b54ae04a653ae28ec32db031597
Sulaiman Sulaiman <SKSulai98@gmail.com>
2024-12-19 16:19:47 +0000

    Year 2024 - Day4 (Part1): Replacing non-matches w/ '.'
    These heatmaps are similar to the one provided for the example
    Producing them for each match made it clear that the diagonal matches are missing duplicates that occur within the frame of an exisiting match

--- 2024/Day04.cpp
+++ 2024/Day04.cpp
@@ -29,6 +29,36 @@ std::size_t matches(const std::string& str, const std::regex& regex) {
     return count;
 }
 
+std::string verification(const std::string& str, const std::regex& regex, bool hasGroups = false) {
+    std::string out;
+    std::regex_replace(std::back_inserter(out), str.begin(), str.end(), regex, hasGroups ? ".$1.$2.$3." : "....");
+    return out;
+}
+
+std::string combineVerificationHeatmap(
+    const std::string& lhs, 
+    const std::string& rhs
+) {
+    std::string out;
+    for(int i = 0; i < lhs.length(); i++) {
+        if(lhs[i] == '.' || rhs[i] == '.')
+            out += '.';
+        else out += lhs[i];
+    }
+    return out;
+}
+
+std::string invertHeatmap(
+    const std::string& original,
+    const std::string& heatmap
+) {
+    std::string out;
+    for(int i = 0; i < original.length(); i++)
+        if(heatmap[i] == '.' || original[i] == '\n') out += original[i];
+        else out += '.';
+    return out;
+}
+
 int main() {
     std::size_t rows = 0, columns = 0;
     std::string elements = "";
@@ -41,15 +71,29 @@ int main() {
         rows++;
     };
 
+    std::cout << "'XMAS' matches:\n" << invertHeatmap(elements, verification(elements, std::regex("XMAS"))) << "\n\n"
+              << "'SAMX' matches:\n" << invertHeatmap(elements, verification(elements, std::regex("SAMX"))) << "\n\n";
 
     std::size_t counts = 0;
+    std::string heatmap = elements;
 
-    counts += matches(elements, std::regex("XMAS"));
-    counts += matches(elements, std::regex("SAMX"));
+    auto find = [&](const std::string& pattern) {
+        auto map = verification(elements, std::regex(pattern), true);
+        heatmap = combineVerificationHeatmap(heatmap, map);
 
-    for(int i = -1; i <= 1; i++) counts += matches(elements, std::regex(std::format(R"(X([\w\n]{{{0}}})M([\w\n]{{{0}}})A([\w\n]{{{0}}})S)", columns + 1 + i)));
-    for(int i = -1; i <= 1; i++) counts += matches(elements, std::regex(std::format(R"(S([\w\n]{{{0}}})A([\w\n]{{{0}}})M([\w\n]{{{0}}})X)", columns + 1 + i)));
+        auto match = matches(elements, std::regex(pattern));
+        counts += match;
 
+        std::cout << std::format("'{}[count: {}]':\n{}\n\n", pattern, match, invertHeatmap(elements, map));
+    };
+    
+    find("XMAS");
+    find("SAMX");
+
+    for(int i = -1; i <= 1; i++) find(std::format(R"(X([\w\n]{{{0}}})M([\w\n]{{{0}}})A([\w\n]{{{0}}})S)", columns + 1 + i));
+    for(int i = -1; i <= 1; i++) find(std::format(R"(S([\w\n]{{{0}}})A([\w\n]{{{0}}})M([\w\n]{{{0}}})X)", columns + 1 + i));
+
+    std::cout << "Combined:\n" << invertHeatmap(elements, heatmap);
 
     std::cout << "Number of XMAS entries: " << counts;
 
